f526485c8e343db872df795f512a8769538ec852
rokoDev <mishaDoter@gmail.com>
2016-07-18 20:48:59 +0300

    first commit

---
+++ NetTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NetTest
+//
+//  Created by roko on 7/15/16.
+//  Copyright © 2016 roko. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
